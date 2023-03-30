389655ee7fd82280fcf832ac87c4e0e457e91558
yww snowboy <ywwsnowboy@outlook.com>
2023-03-30 19:40:12 +0800

    update files

--- Extract_images.py
+++ Extract_images.py
@@ -10,19 +10,16 @@ def download_images(path):
         matches = re.findall(r"!\[.*\]\((.+)\)", content)
         for url in matches:
             if url.startswith("http") or url.startswith("https"):
-                asset_path = os.path.join(os.path.dirname(path), 'assets', os.path.basename(url))
+                asset_path = os.path.join(
+                    os.path.dirname(path), 'assets', os.path.basename(url))
                 if not os.path.exists(os.path.dirname(asset_path)):
                     os.makedirs(os.path.dirname(asset_path))
                 if not os.path.exists(asset_path):
                     with requests.get(url, stream=True) as r, open(asset_path, 'wb') as f:
                         shutil.copyfileobj(r.raw, f)
-                content = content.replace(url, f"./assets/{os.path.basename(url)}")
-            # else:
-            #     asset_path = os.path.join(os.path.dirname(path), url)
-            #     if not os.path.exists(os.path.dirname(asset_path)):
-            #         os.makedirs(os.path.dirname(asset_path))
-            #     shutil.copyfile(url, asset_path)
-            #     content = content.replace(url, f"./{os.path.basename(url)}")
+                content = content.replace(
+                    url, f"./assets/{os.path.basename(url)}")
+
         with open(path, 'w', encoding='utf-8') as f:
             f.write(content)
 
@@ -46,14 +43,4 @@ if not os.path.exists(new_dir):
 
 shutil.copytree(old_dir, new_dir, dirs_exist_ok=True)
 
-batch_download_images('./new/')
-
-
-
-
-
-
-
-
-
-
+batch_download_images(new_dir)
